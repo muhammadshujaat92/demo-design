1b9b8ba844ed569a437162ebcbd6b868615cc7d4
muhammadshujaat92 <muhammadshujaat04@gmail.com>
2024-09-09 06:47:32 +0500

    updated import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ReduxProvider from "./_redux/reduxProvider";
+import ReduxProvider from "./_redux/ReduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
